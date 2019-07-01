66e1a523a68e75cd5fd9d2b667ff21d3ed25849d
Diana <d_quinlan061006@yahoo.com>
2019-06-30 22:30:29 -0400

    appending scraped articles, need to work on saving and notes

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -5,7 +5,7 @@ var display = function(){
         console.log(data);
         for (var i = 0; i < data.length; i++) {
             console.log(data[i]);
-            $("#articles-well").append("<div class ='card'"+ "<div class='card-body'>" +"<h5 class='card-title'>"+ data[i].title +"</h5>" +"<br>" + "<a href='"+ data[i].link +"'>Article Link</a>"+ "</div>");
+            $("#articles-well").append("<div class ='card'"+ "<div class='card-body'>" +"<h5 class='card-title'>"+ data[i].title +"</h5>"+ "<a href='"+ data[i].link +"'>Article Link</a>"+ "<br><div><button type='button' class='btn btn-success'id='save'>Save</button></div><br></div></div>"+"<br>");
         }
     });
 }
@@ -14,3 +14,9 @@ var display = function(){
       display();
     });
 
+// save on click
+// click save button and it tags the article by the id, set route to saved articles page here? 
+
+// click saved articles link to direct to saved handlebars file
+
+// on saved articles file option to remove saved article, add a note and save to notes table, button to remove note
